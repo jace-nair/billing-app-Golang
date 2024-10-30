a7173845e908fa683e84664c4f9bdd7fa59c380f
Jace <contactjacenair@gmail.com>
2024-10-30 05:11:43 +0000

    Add Arrays and Slices

--- test.go
+++ test.go
@@ -9,6 +9,13 @@ func main() {
 	var myFloatNumber float64 = 2.33
 	const myConst string = "This is a const string"
 
+	var myArray [3]int = [3]int{20, 25, 30}
+	var anotherArray [2]int = [2]int{4, 5}
+
+	var mySlice []int = []int{1, 2, 3}
+	var mySlice2 = []int{1, 2, 3, 4, 5}
+	mySlice3 := []int{1, 2, 3, 4, 5, 6}
+
 	fmt.Println(myString)
 	fmt.Println(myNumber)
 	fmt.Println(myFloatNumber)
@@ -19,5 +26,9 @@ func main() {
 
 	fmt.Println(myString)
 	fmt.Println(myNumber)
+
+	fmt.Printf("%v and %v. And %v is an integer while %v is a float\n", myString, myConst, myNumber, myFloatNumber)
+
+	fmt.Println(myArray, len(myArray), anotherArray, len(anotherArray))
 
 }
