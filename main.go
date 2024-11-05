3dd5395ed0cdd546569c885e9eaef0e8657db0e9
Jace <contactjacenair@gmail.com>
2024-11-05 22:05:00 +0000

    Add helper functions to simplify the  business logic

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Helper function that returns the user input along with any error
+func getInput(prompt string, r *bufio.Reader) (string, error) { // accepts a prompt and a reader r and returns string and error
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')     // reader reads everything before new line or enter
+	return strings.TrimSpace(input), err //get rid of any white space and return the string and the error
+}
+
+// Create a new bill with user input. Returns a bill object.
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin) //create a reader using bufio package's NewReader method and read from Stdin (terminal)
+
+	//fmt.Print("Create a new bill name: ")
+	//name, _ := reader.ReadString('\n') // reader reads everything before new line or enter
+	//name = strings.TrimSpace(name)     //get rid of any white space
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name) // create a new bill with name passed in
+	fmt.Println("Created the bill - ", b.name)
+	return b // return the bill
+}
+
+func promptOptions(b bill) { //giving user prompt options to add an item, tip or save the file
+	reader := bufio.NewReader((os.Stdin)) //local varible
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
 
 func main() {
 
+	//Version 1
+
 	// Create a new instance of the bill object
-	harrysBill := newBill("Harry's bill")
+	//harrysBill := newBill("Harry's bill")
 
 	// Add Items
-	harrysBill.addItem("Onion soup", 4.50)
-	harrysBill.addItem("Veg Pie", 8.95)
-	harrysBill.addItem("toffee pudding", 4.95)
-	harrysBill.addItem("Coffee", 3.25)
+	//harrysBill.addItem("Onion soup", 4.50)
+	//harrysBill.addItem("Veg Pie", 8.95)
+	//harrysBill.addItem("toffee pudding", 4.95)
+	//harrysBill.addItem("Coffee", 3.25)
 
 	// Update tip
-	harrysBill.updateTip(10)
+	//harrysBill.updateTip(10)
 
 	//Print a formatted Harry's bill
-	fmt.Println(harrysBill.format())
+	//fmt.Println(harrysBill.format())
+
+	mybill := createBill()
+	promptOptions(mybill)
+
 }
